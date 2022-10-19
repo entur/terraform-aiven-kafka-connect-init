e20fecdefbdf51328cd150128ad33d61bf6d1979
Upendra Vedullapalli <upendra.rao.vedullapalli@entur.org>
2022-10-19 22:49:14 +0200

    touch for next release

--- examples/init/main.tf
+++ examples/init/main.tf
@@ -2,7 +2,7 @@
 
 variable "api_token" {}
 module "init" {
-  source         = "github.com/entur/terraform-aiven-kafka-connect-init/modules/init?ref=v1.0.2"
+  source         = "github.com/entur/terraform-aiven-kafka-connect-init/modules/init?ref=vVERSION"
   access_token   = var.api_token
   project_name   = "my-aiven-project-name"
   service_name   = "my-aiven-service-name"
