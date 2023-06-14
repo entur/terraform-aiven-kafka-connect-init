8ccbf29c846f5d76e9f50c6e53fa5f18d28a5f67
Upendra Vedullapalli <upendra.rao.vedullapalli@entur.org>
2023-06-14 14:13:41 +0200

    fix: exporting proper kafka service name and kafka-connect service names

--- examples/init/main.tf
+++ examples/init/main.tf
@@ -5,10 +5,10 @@ module "init" {
   # This is an example only; if you're adding this block to a live configuration,
   # make sure to use the latest release of the init module, found here:
   # https://github.com/entur/terraform-aiven-kafka-connect-init/releases
-  source       = "github.com/entur/terraform-aiven-kafka-connect-init//modules/init?ref=v1.1.3"
-  access_token = var.api_token
-  project_name = "my-aiven-project-name"
-  service_name = "my-aiven-service-name"
+  source             = "github.com/entur/terraform-aiven-kafka-connect-init//modules/init?ref=v1.1.3"
+  access_token       = var.api_token
+  project_name       = "my-aiven-project-name"
+  kafka_service_name = "my-aiven-kafka-service-name"
 }
 
 # ci: x-release-please-end
--- modules/init/main.tf
+++ modules/init/main.tf
@@ -4,17 +4,18 @@ data "aiven_project" "entur-aiven-project" {
 
 data "aiven_service_component" "schema-registry" {
   project      = data.aiven_project.entur-aiven-project.project
-  service_name = var.service_name
+  service_name = var.kafka_service_name
   component    = "schema_registry"
   route        = "dynamic"
 }
 
 locals {
   aiven = {
-    access_token        = var.access_token
-    project             = data.aiven_project.entur-aiven-project.project
-    service             = var.service_name
-    schema_registry_url = "https://${data.aiven_service_component.schema-registry.host}:${data.aiven_service_component.schema-registry.port}"
+    access_token         = var.access_token
+    project              = data.aiven_project.entur-aiven-project.project
+    kafka_service_name   = var.kafka_service_name
+    connect_service_name = join("-", [var.kafka_service_name, "connect"])
+    schema_registry_url  = "https://${data.aiven_service_component.schema-registry.host}:${data.aiven_service_component.schema-registry.port}"
   }
   default_configuration = {
     "tasks.max" : var.tasks_max,
